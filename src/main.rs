7f2b181d53a488551285c4c8ea97be82ac217949
alessandro90 <alessandro.ceccato.3@gmail.com>
2023-11-28 23:34:24 +0100

    Return ExitCode from the main function to signal failure

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
-use std::env;
+use std::{env, process::ExitCode};
 
 use brainfuck_rs::runners::{run_file, run_repl};
 
-fn main() {
+fn main() -> ExitCode {
     let args: Vec<_> = env::args().collect();
     if args.len() == 1 {
         run_repl();
-    } else {
-        if args.len() > 2 {
-            eprintln!("Unused command arguments: {:?}", &args[2..]);
-        }
-        let fname = args[1].as_str();
-        run_file(fname);
+        return ExitCode::SUCCESS;
     }
+    if args.len() > 2 {
+        eprintln!("Unused command arguments: {:?}", &args[2..]);
+        return ExitCode::FAILURE;
+    }
+    let fname = args[1].as_str();
+    run_file(fname)
 }
--- src/runners.rs
+++ src/runners.rs
@@ -1,6 +1,7 @@
 use std::{
     fs,
     io::{self, Write},
+    process::ExitCode,
 };
 
 use crate::brainfuck::{Interpreter, Lexer};
@@ -9,11 +10,13 @@ pub fn run_repl() {
     let mut input = String::new();
     let mut lexer = Lexer::default();
     let mut interpreter = Interpreter::default();
+    let mut line_nr = 0;
     loop {
-        print!("$ ");
+        print!("[{}] ", line_nr);
         io::stdout().flush().unwrap();
         io::stdin().read_line(&mut input).expect("Invalid input");
         if input.is_empty() || input.trim().ends_with(';') {
+            line_nr += 1;
             continue;
         }
         lexer.append(&input);
@@ -28,12 +31,20 @@ pub fn run_repl() {
     }
 }
 
-pub fn run_file(fname: &str) {
-    let src_code = fs::read_to_string(fname)
-        .unwrap_or_else(|e| panic!("Cannot read file {}. Error: {}", fname, e));
-    let mut lexer = Lexer::from(&src_code);
-    let mut interpreter = Interpreter::default();
-    if let Err(e) = interpreter.interpret(&mut lexer) {
-        eprintln!("Program exited with error: {:?}", e);
+pub fn run_file(fname: &str) -> ExitCode {
+    match fs::read_to_string(fname) {
+        Err(e) => {
+            eprintln!("Cannot read file {}. Error: {}", fname, e);
+            ExitCode::FAILURE
+        }
+        Ok(src_code) => {
+            let mut lexer = Lexer::from(&src_code);
+            let mut interpreter = Interpreter::default();
+            if let Err(e) = interpreter.interpret(&mut lexer) {
+                eprintln!("Program exited with error: {:?}", e);
+                return ExitCode::FAILURE;
+            }
+            ExitCode::SUCCESS
+        }
     }
 }
